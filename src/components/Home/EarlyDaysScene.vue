6d72b7e46f15999eda7c3fbe3b79bda3167cca3d
nielcabs <edgarrfajar123@gmail.com>
2023-03-28 11:50:41 +0800

    Tang ina init ng Laptop

---
+++ src/components/Home/EarlyDaysScene.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="earlyDays">
+    <TitleSection scene="earlyTitle">
+      <TitleFunction
+        params="2022,2023"
+        subtitle="Own Software Developing Page"
+        >UndeciDev</TitleFunction
+      >
+
+      <div class="clouds cloud-1"></div>
+      <Pepe v-bind="{ isPlaying }" />
+      <div class="clouds cloud-2"></div>
+      <div class="clouds cloud-3"></div>
+      <Ocean v-bind="{ isPlaying }" />
+    </TitleSection>
+
+    <SceneSection id="early-days">
+      <TextBlock>
+        <p>Web Development & Game Development.</p>
+        <p class="-big">
+          A Full Stack Developer<br />
+          <span class="-purple">with putting challenge on its developing.</span>
+        </p>
+      </TextBlock>
+    </SceneSection>
+    <SceneSection id="early-days2">
+      <TextBlock>
+        <p class="-big">
+          <span class="-purple">Freelancing</span> &
+          <span class="-purple">Startuping,</span><br />
+          <span class="-purple">Develop</span> &
+          <span class="-purple">Design</span>
+        </p>
+        <p>
+          Making Web app Game, developing small projects and focusing to improve my programming skills.<br />
+        </p>
+      </TextBlock>
+    </SceneSection>
+    <SceneSection id="early-days3">
+      <TextBlock>
+        <p>
+          I'am proud to develop games<br />
+          that anyone can play like
+          <span class="-big -purple"
+            ><em>Sinigang</em>, <em>Kyubix</em>, Stack with you,
+            <em>The Hospital</em></span
+          >
+          among many others...
+        </p>
+      </TextBlock>
+    </SceneSection>
+  </div>
+</template>
+
+<script>
+import Pepe from '../Characters/Pepe/Pepe.vue'
+import Ocean from '../Characters/Ocean/Ocean.vue'
+import TitleSection from '../TitleSection.vue'
+import TitleFunction from '../TitleFunction.vue'
+import TextBlock from '../TextBlock.vue'
+import SceneSection from '../SceneSection.vue'
+
+export default {
+  name: 'EarlyDaysScene',
+  components: {
+    TitleSection,
+    Pepe,
+    Ocean,
+    TitleFunction,
+    TextBlock,
+    SceneSection,
+  },
+  props: {
+    isPlaying: Boolean,
+  },
+}
+</script>
+
+<style lang="scss">
+.earlyDays {
+  .pepe {
+    position: absolute;
+    z-index: 2;
+    top: 50%;
+    left: 100%;
+    transform: scale(0.4) translate3d(0, -50%, 0);
+    transform-origin: 0 0;
+
+    @media screen and (max-width: 568px) {
+      transform: scale(0.2) translate3d(0, -50%, 0);
+    }
+  }
+
+  .clouds {
+    position: absolute;
+    width: 150vw;
+    height: 150vh;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    transform-origin: 50% 50%;
+    background: url('../Characters/Pepe/assets/clouds-1.png') no-repeat center;
+    background-size: contain;
+    opacity: 0.5;
+
+    &.cloud-2 {
+      background-image: url('../Characters/Pepe/assets/clouds-2.png');
+    }
+
+    &.cloud-3 {
+      background-image: url('../Characters/Pepe/assets/clouds-3.png');
+      width: 100vw;
+      height: 100vh;
+      z-index: -1;
+      opacity: 0.25;
+    }
+  }
+}
+</style>
---
+++ src/components/Home/IntroScene.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="scene-intro" id="intro">
+    <div class="static-container">
+      <h1 class="title">
+        <span class="iuri">Undeci</span>
+        <span class="func">Dev(?)</span>
+      </h1>
+
+      <TextBlock>
+        <p class="-purple">Juniel Cabo</p>
+        <p class="-gray">Full Stack Developer</p>
+      </TextBlock>
+    </div>
+  </section>
+</template>
+
+<script>
+import TextBlock from '../TextBlock.vue'
+
+export default {
+  name: 'IntroScene',
+  components: { TextBlock },
+}
+</script>
